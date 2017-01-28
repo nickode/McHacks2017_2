bc522b9bb142fbfc0039c151846c0ecf1d2bf694
edward-son <edward_son@live.com>
2017-01-28 16:01:20 -0500

    test

--- src/application/Main.java
+++ src/application/Main.java
@@ -3,7 +3,10 @@ package application;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
@@ -14,6 +17,10 @@ public class Main extends Application {
 	
 	Button button1,button2,button3;
 	Label label1,label2;
+	int rowNum = 6;
+	int colNum = 6;
+	int gridHeight = 6;
+	int gridWidth = 6;
 
 	
 	public void start(Stage primaryStage) {
@@ -21,7 +28,7 @@ public class Main extends Application {
 			//scene 1
 			primaryStage.setTitle("Picture Game");
 			StackPane layout = new StackPane();
-			Scene scene = new Scene(layout,400,400);
+			Scene scene = new Scene(layout,600,4000);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			
 			button1 = new Button();
@@ -41,13 +48,28 @@ public class Main extends Application {
 			primaryStage.show();
 			
 			//scene 2
-			StackPane layout2 = new StackPane();
-			Scene scene2 = new Scene(layout2,400,400);
+			GridPane grid = new GridPane();
 			
-			ImageView pic1 = new ImageView(new Image(getClass().getResourceAsStream("/res/salt.jpg")));
-			layout2.getChildren().add(pic1);
+			for (int row=0;row<rowNum;row++){
+				for(int col=0;col<colNum;col++){
+					Rectangle r = new Rectangle();
+					r.setWidth(100);
+					r.setHeight(100);
+					r.setFill(Color.ALICEBLUE);
+					r.setStroke(Color.BLACK);
+					GridPane.setRowIndex(r, row);
+					GridPane.setColumnIndex(r, col);
+					grid.getChildren().addAll(r);
+					r.widthProperty().bind(grid.widthProperty().divide(rowNum));
+				}
+			}
+			//StackPane layout2 = new StackPane();
+			Scene scene2 = new Scene(grid,600,4000);
+			
+			//ImageView pic1 = new ImageView(new Image(getClass().getResourceAsStream("/res/salt.jpg")));
+			//grid.getChildren().add(pic1);
 			button3 = new Button("Quit Game");
-			layout2.getChildren().add(button3);
+			grid.getChildren().add(button3);
 			
 			button3.setOnAction(e -> primaryStage.setScene(scene));
 			
